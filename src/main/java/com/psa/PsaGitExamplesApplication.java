55e91a11ad04212b6dde061648fc45a07cb36c77
Gopichand <j.gopichandyadav@gmail.com>
2025-03-10 14:38:22 +0530

    First Commit

---
+++ src/main/java/com/psa/PsaGitExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.psa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsaGitExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsaGitExamplesApplication.class, args);
+	}
+
+}
