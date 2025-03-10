be884ea26da104d81fb2f91bbc90470897ed9ccf
virushka-18 <j.gopichandyadav@gmail.com>
2025-03-10 14:42:51 +0530

    Update PsaGitExamplesApplication.java

--- src/main/java/com/psa/PsaGitExamplesApplication.java
+++ src/main/java/com/psa/PsaGitExamplesApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PsaGitExamplesApplication {
 
 	public static void main(String[] args) {
+		int x = 100;
 		SpringApplication.run(PsaGitExamplesApplication.class, args);
 	}
 
